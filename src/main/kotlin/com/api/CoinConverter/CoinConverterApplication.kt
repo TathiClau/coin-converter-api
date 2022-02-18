29b6da79ea7d7c953b14a09eb7cd29ee25b87e5d
Tathi <tathiane.claudia@gmail.com>
2022-02-17 21:51:46 -0300

    feat: Generate spring initializer

---
+++ src/main/kotlin/com/api/CoinConverter/CoinConverterApplication.kt
@@ -0,0 +1,11 @@
+package com.api.CoinConverter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoinConverterApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoinConverterApplication>(*args)
+}
---
+++ src/test/kotlin/com/api/CoinConverter/CoinConverterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api.CoinConverter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoinConverterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
