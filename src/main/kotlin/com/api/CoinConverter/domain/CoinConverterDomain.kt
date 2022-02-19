a5715e72fe7523497c182087977ce911d11f9637
Tathi <tathiane.claudia@gmail.com>
2022-02-19 16:09:50 -0300

    feature: Build cosume rates service

--- src/main/kotlin/com/api/CoinConverter/domain/CoinConverterDomain.kt
+++ src/main/kotlin/com/api/CoinConverter/domain/CoinConverterDomain.kt
@@ -1,12 +1,18 @@
 package com.api.CoinConverter.domain
 
+import java.sql.Timestamp
 import java.util.*
-
-
 
 data class AddCoinConversion (val accessKey: String,
                               val userId: UUID,
                               val initialCoin: String,
                               val initialValue: Double,
                               val finalCoin: String)
+
+
+data class ResponseConversionRate (val success: Boolean,
+                                   val timestamp: Timestamp,
+                                   val base: String,
+                                   val date: Date,
+                                   val rates: Map<String, Double>)
 
